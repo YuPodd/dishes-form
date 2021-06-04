160563ed55ca7789a5dee81d17590b302ade7bec
Yuliya <yulia.podd@gmail.com>
2021-06-04 17:29:55 +0300

    feat: add onChange dishes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import { useState } from "react";
-import { Form, Input, Button, Select } from "antd";
+import { Form, Input, Button, Select, Slider } from "antd";
 import "antd/dist/antd.css";
 import "./index.css";
 import "animate.css/animate.css";
+import FormItemLabel from "antd/lib/form/FormItemLabel";
 
 function App() {
   const { Option } = Select;
-  const [formLayout, setFormLayout] = useState('vertical');
+  const [formLayout, setFormLayout] = useState("vertical");
+  const [formItemName, setFormItemName] = useState("");
+  const [formItemLabel, setFormItemLabel] = useState("");
+
+  const onDishChange = (value) => {
+    switch (value) {
+      case "pizza":
+        console.log("pizza");
+        setFormItemName("slices");
+        setFormItemLabel("Number of Slices");
+        return;
+
+      case "sandwich":
+        console.log("sandwich");
+        setFormItemName("spiceness");
+        setFormItemLabel("Spiceness");
+        return;
+
+      case "soup":
+        console.log("soup");
+        setFormItemName("slices");
+        setFormItemLabel("Number of Slices");
+        return;
+    }
+    console.log("1st time");
+  };
   return (
     <>
       <main className="form-wrapper">
-        <Form name="control-ref" layout={formLayout} className="animate__animated animate__fadeInDown">
+        <Form
+          name="control-ref"
+          layout={formLayout}
+          className="animate__animated animate__fadeInDown"
+        >
           <Form.Item
             name="name"
             label="Name"
@@ -31,7 +61,7 @@ function App() {
               },
             ]}
           >
-             <Input />
+            <Input />
           </Form.Item>
           <Form.Item
             name="dish"
@@ -42,13 +72,21 @@ function App() {
               },
             ]}
           >
-            <Select placeholder="Select a dish type" allowClear>
+            <Select
+              placeholder="Select a dish type"
+              allowClear
+              onChange={onDishChange}
+            >
               <Option value="pizza">Pizza</Option>
               <Option value="soup">Soup</Option>
               <Option value="sandwich">Sandwich</Option>
             </Select>
           </Form.Item>
-
+          {formItemName !== "" ? (
+            <Form.Item name={formItemName} label={formItemLabel}>
+              {FormItemLabel}
+            </Form.Item>
+          ) : null}
           <Form.Item>
             <Button type="primary" htmlType="submit">
               Submit
